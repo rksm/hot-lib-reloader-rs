52fc86a5d6d34cd8bf8bd17de9fa0ae1d9864e39
Robert Krahn <robert.krahn@gmail.com>
2022-08-01 01:38:45 +0200

    make it work on windows

--- src/lib.rs
+++ src/lib.rs
@@ -117,76 +117,76 @@ use std::time::Instant;
 pub struct LibReloader {
     changed: Arc<atomic::AtomicBool>,
     changed_at: Arc<Mutex<Option<Instant>>>,
-    lib: Library,
-    lib_file: PathBuf,
-    last_loaded_lib_file: Option<PathBuf>,
-    reload_counter: usize,
+    lib: Option<Library>,
+    watched_lib_file: PathBuf,
+    loaded_lib_file: PathBuf,
 }
 
 impl LibReloader {
     pub fn new(
         lib_dir: impl AsRef<Path>,
-        lib_name: impl AsRef<Path>,
+        lib_name: impl AsRef<str>,
     ) -> Result<Self, Box<dyn Error>> {
         #[cfg(target_os = "macos")]
-        let ext = "dylib";
+        let (prefix, ext) = ("lib", "dylib");
         #[cfg(target_os = "linux")]
-        let ext = "so";
-        let lib_file = lib_dir.as_ref().join(lib_name).with_extension(ext);
-        let lib = unsafe { Library::new(&lib_file) }?;
+        let (prefix, ext) = ("lib", "so");
+        #[cfg(target_os = "windows")]
+        let (prefix, ext) = ("", "dll");
+        let lib_name = format!("{prefix}{}", lib_name.as_ref());
+        let watched_lib_file = lib_dir.as_ref().join(&lib_name).with_extension(ext);
+        let loaded_lib_file = lib_dir
+            .as_ref()
+            .join(format!("{lib_name}-hot"))
+            .with_extension("dll");
+        // We don't load the actual lib because this can get problems e.g. on Windows
+        // where a file lock would be held, preventing the lib from changing later.
+        std::fs::copy(&watched_lib_file, &loaded_lib_file)?;
+        let lib = Some(unsafe { Library::new(&loaded_lib_file) }?);
+
         let lib_loader = Self {
-            lib_file: lib_file.clone(),
+            watched_lib_file: watched_lib_file.clone(),
+            loaded_lib_file,
             lib,
-            last_loaded_lib_file: None,
             changed: Arc::new(atomic::AtomicBool::new(false)),
             changed_at: Arc::new(Mutex::new(None)),
-            reload_counter: 0,
         };
 
-        lib_loader.watch(lib_file)?;
+        lib_loader.watch(watched_lib_file)?;
 
         Ok(lib_loader)
     }
 
-    pub fn update(&mut self) -> Result<(), Box<dyn Error>> {
-        if self.changed.load(Ordering::Relaxed) {
-            self.changed.store(false, Ordering::Relaxed);
-            self.reload()?;
-            let mut changed_at = self.changed_at.lock().unwrap();
-            *changed_at = Some(Instant::now());
+    /// Checks if the watched library has changed. If it has, reload it and return
+    /// true. Otherwise return false.
+    pub fn update(&mut self) -> Result<bool, Box<dyn Error>> {
+        if !self.changed.load(Ordering::Relaxed) {
+            return Ok(false);
         }
-        Ok(())
+        self.changed.store(false, Ordering::Relaxed);
+        self.reload()?;
+        let mut changed_at = self.changed_at.lock().unwrap();
+        *changed_at = Some(Instant::now());
+        Ok(true)
     }
 
     /// Reload library `self.lib_file`.
     fn reload(&mut self) -> Result<(), Box<dyn Error>> {
-        // step 1: copy the base lib file to a new lib file with a unique name
-        self.reload_counter += 1;
-        let counter = self.reload_counter;
-        let base_file = self.lib_file.with_extension("");
-        let ext = self
-            .lib_file
-            .extension()
-            .unwrap_or_default()
-            .to_string_lossy();
-        let new_lib_file = format!("{}-{counter}.{ext}", base_file.display()).into();
-        std::fs::copy(&self.lib_file, &new_lib_file)?;
+        let Self {
+            lib,
+            watched_lib_file,
+            loaded_lib_file,
+            ..
+        } = self;
 
-        // step 2: load the new lib file and close the old one
-        println!(
-            "[hot-lib-reloader] reloading lib {} from file {new_lib_file:?}",
-            self.lib_file.display()
-        );
-        self.lib = unsafe { Library::new(&new_lib_file) }?;
+        println!("[hot-lib-reloader] reloading lib {watched_lib_file:?}",);
 
-        // step 3: if we were running on a reloaded lib, remove that to not
-        // leave files sitting around
-        let last_loaded_lib_file = self.last_loaded_lib_file.replace(new_lib_file);
-        if let Some(last_loaded_lib_file) = last_loaded_lib_file {
-            if last_loaded_lib_file.exists() {
-                std::fs::remove_file(last_loaded_lib_file)?;
-            }
+        // Close the loaded lib, copy the new lib to a file we can load, then load it.
+        if let Some(lib) = lib.take() {
+            lib.close()?;
         }
+        std::fs::copy(watched_lib_file, loaded_lib_file)?;
+        self.lib = Some(unsafe { Library::new(&self.loaded_lib_file) }?);
 
         Ok(())
     }
@@ -201,10 +201,19 @@ impl LibReloader {
 
         let changed = self.changed.clone();
         let last_changed = self.changed_at.clone();
+
+        // File watcher thread. We watch `self.lib_file`, when it changes and we haven't
+        // a pending change still waiting to be loaded, set `self.changed` to true. This
+        // then gets picked up by `self.update`.
         thread::spawn(move || {
             use DebouncedEvent::*;
 
             let (tx, rx) = mpsc::channel();
+            let mut watcher = watcher(tx, Duration::from_millis(50)).unwrap();
+            watcher
+                .watch(&lib_file, RecursiveMode::NonRecursive)
+                .expect("watch lib file");
+
             let debounce = Duration::from_millis(500);
             let mut last_change = Instant::now() - debounce;
             let mut signal_change = || {
@@ -217,7 +226,7 @@ impl LibReloader {
                     .map(|t| now - t < debounce)
                     .unwrap_or(false)
                 {
-                    return;
+                    return false;
                 }
 
                 log::debug!("[hot-lib-reloader] {} changed", lib_file.display());
@@ -225,11 +234,8 @@ impl LibReloader {
                 last_change = now;
                 changed.store(true, Ordering::Relaxed);
                 last_changed.lock().unwrap().replace(Instant::now());
+                true
             };
-            let mut watcher = watcher(tx, Duration::from_millis(50)).unwrap();
-            watcher
-                .watch(&lib_file, RecursiveMode::NonRecursive)
-                .expect("watch lib file");
 
             loop {
                 match rx.recv() {
@@ -283,16 +289,14 @@ impl LibReloader {
     ///
     /// Users of this API must specify the correct type of the function or variable loaded.
     pub unsafe fn get_symbol<T>(&self, name: &[u8]) -> Result<Symbol<T>, Box<dyn Error>> {
-        Ok(self.lib.get(name)?)
+        Ok(self.lib.as_ref().unwrap().get(name)?)
     }
 }
 
 impl Drop for LibReloader {
     fn drop(&mut self) {
-        if let Some(last_loaded_lib_file) = self.last_loaded_lib_file.take() {
-            if last_loaded_lib_file.exists() {
-                let _ = std::fs::remove_file(last_loaded_lib_file);
-            }
+        if self.loaded_lib_file.exists() {
+            let _ = std::fs::remove_file(&self.loaded_lib_file);
         }
     }
 }
