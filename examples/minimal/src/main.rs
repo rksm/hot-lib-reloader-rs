98bb42f3007e8903dabf8b22087f2339f5af2fea
Robert Krahn <robert.krahn@gmail.com>
2022-08-08 23:29:50 +0200

    less minimal minimal example

--- examples/minimal/src/main.rs
+++ examples/minimal/src/main.rs
@@ -1,11 +1,13 @@
 #[hot_lib_reloader::hot_module(dylib = "lib")]
 mod hot_lib {
+    pub use lib::State;
     hot_functions_from_file!("../lib/src/lib.rs");
 }
 
 fn main() {
+    let mut state = hot_lib::State { counter: 0 };
     loop {
-        hot_lib::do_stuff();
+        hot_lib::do_stuff(&mut state);
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
